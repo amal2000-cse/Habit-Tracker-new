70e8a1ded8e85b3d4755159419e645cdef3ec93b
amal2000-cse <kannothparambathamalmanoj@gmail.com>
2023-12-17 18:56:02 +0530

    update

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 
+
 // Connecting to the MongoDB database
-const url = "mongodb+srv://amal:suvarnam123@cluster0.rc1clne.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const url="mongodb+srv://amal:suvarnam123@cluster0.rc1clne.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(url);
 
 const db = mongoose.connection;
 
 // Event listener for database connection error
-db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+db.on('error', console.error.bind(console, 'Error connecting to the database'));
 
 // Event listener for successful database connection
-db.once('open', function () {
+db.once('open', function(){
     console.log('Database connection successful');
 });
-
-module.exports = mongoose; // Export the mongoose instance
